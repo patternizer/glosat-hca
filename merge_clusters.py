9b33ae4f0fa4c0de2d77fd84ea525de563677412
patternizer <michael.a.taylor@uea.ac.uk>
2022-06-09 16:01:59 +0100

    Sort reconstructed clusters by stationcode and year

--- merge_clusters.py
+++ merge_clusters.py
@@ -2,8 +2,9 @@ import os
 import glob
 import pandas as pd
 
-pkllist = glob.glob("*.pkl")
-df_temp_expect = pd.concat([pd.read_pickle(pkllist[i], compression='bz2') for i in range(len(pkllist))])
-df_temp_expect.to_pickle('df_temp_reconstructed.pkl', compression='bz2')
+pkllist = glob.glob( "cluster-pkl/*.pkl" )
+df_temp_reconstructed = pd.concat( [ pd.read_pickle( pkllist[i], compression='bz2' ) for i in range( len( pkllist ) ) ])
+df_temp_reconstructed = df_temp_reconstructed.sort_values(['stationcode','year'], ascending=[True, True]).reset_index(drop=True)
+df_temp_reconstructed.to_pickle( 'df_temp_reconstructed.pkl', compression='bz2' ) 
 
 
